3e4e3ffe2cf4806b6940f8f27eddc54de5e9d089
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-29 18:59:58 +0200

    defimos la función

--- fumador.py
+++ fumador.py
@@ -13,7 +13,25 @@ cerilla_sem = threading.Semaphore(1) #El agente tiene la cerilla
 
 def agente():
     while True:
+        #El agente coloca dos ingredientes aleatorios en la mesa
+        ingredientes = random.sample(['papel', 'tabaco', 'filtros', 'green'], 2) #el 2 se debe a que en el enunciado se pide que coloque dos ingredientes
+        print("Agente coloca en la mesa: ", ingredientes)
+        #Avisa a los fumadores de que hay nuevos ingredientes disponibles
+        if 'papel' in ingredientes and 'tabaco' in ingredientes: #si en la lista ingredientes hay papel y tabaco
+            filtros_sem.release() #libera el semaforo de filtros
+        elif 'papel' in ingredientes and 'filtros' in ingredientes:
+            tabaco_sem.release()
+        elif 'papel' in ingredientes and 'green' in ingredientes:
+            cerilla_sem.release()
+        elif 'tabaco' in ingredientes and 'filtros' in ingredientes:
+            papel_sem.release()
+        elif 'tabaco' in ingredientes and 'green' in ingredientes:
+            cerilla_sem.release()
+        elif 'filtros' in ingredientes and 'green' in ingredientes:
+            tabaco_sem.release()
+        time.sleep(1)
 
+       
 
 
 
